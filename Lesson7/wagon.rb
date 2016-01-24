a4a44aae31a88939d379777d60d2e53eae95dd09
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-01-24 10:44:58 +0300

    Добавить метод, возвращающий кол-во свободных мест в вагоне.

--- Lesson7/wagon.rb
+++ Lesson7/wagon.rb
@@ -6,12 +6,12 @@ require_relative 'producer'
 class Wagon
 	include Producer
 	
-	attr_accessor :type
-	attr_accessor :seats_count
+	attr_accessor :type, :total_seats
+	attr_reader :taken_seats
 	
-	def initialize(type, seats_count)
+	def initialize(type, total_seats)
 		@type = type
-		@seats_count = seats_count
+		@seats_count = total_seats
 		
 		validate!
 	end
@@ -22,7 +22,17 @@ class Wagon
 		false
 	end
 	
+	def take_seat
+		self.taken_seats += 1 if self.taken_seats <  self.total_seats
+	end
+	
+	def free_seats
+		self.total_seats - self.taken_seats
+	end
+	
 	protected 
+	
+	attr_writer :taken_seats
 	
 	def validate!
 		raise "type" if type.to_s == ''
