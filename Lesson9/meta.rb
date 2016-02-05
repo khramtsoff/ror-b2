9b0d20045578821be5393a9d7183028b3c9771b9
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-02-05 23:37:40 +0300

    метод strong_attr_acessor, который принимает имя атрибута и его класс. При этом создается геттер и сеттер для одноименной инстанс-переменной, но сеттер проверяет тип присваемоего значения. Если тип отличается от того, который указан вторым параметром, то выбрасывается исключение. Если тип совпадает, то значение присваивается. Подключить модуль в класс и продемонстрировать применение этих методов.

--- Lesson9/meta.rb
+++ Lesson9/meta.rb
@@ -1,55 +1,65 @@
 #!/usr/bin/ruby
 
 module Meta
-	def self.included(base)
-		base.extend ClassMethods
-		base.send :include, InstanceMethods
-	end
-	
-	module ClassMethods
-		def att_accessor_with_history(*args)
-			args.each do |name|
-				define_method("#{name}=") do |val|
-					instance_variable_set("@#{name}", val)
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    def att_accessor_with_history(*args)
+      args.each do |name|
+        define_method("#{name}=") do |val|
+          instance_variable_set("@#{name}", val)
           h = instance_variable_get("@#{name}_history")
           h ||= []
           h << val
           instance_variable_set("@#{name}_history", h)
-				end
-				define_method("#{name}") do
-					instance_variable_get("@#{name}")
-				end
+        end
+
+        define_method("#{name}") do
+          instance_variable_get("@#{name}")
+        end
 
         define_method("#{name}_history") do
           instance_variable_get("@#{name}_history")
         end
-			end
-		end
-		
-		def strong_attr_acessor
-		end
-	end
-	
-	module InstanceMethods
-	end
+      end
+    end
+
+
+    def strong_attr_acessor(name, cl)
+      define_method("#{name}=") do |val|
+        raise "Wrong class" unless val.instance_of? cl
+        instance_variable_set("@#{name}", val)
+      end
+
+      define_method("#{name}") do
+        instance_variable_get("@#{name}")
+      end
+    end
+  end
+
+  module InstanceMethods
+  end
 end
 
 
 class Test
-	include Meta
-	
+  include Meta
+
 end
 
 
 module Validation
-	def validate
-	end
-	
-	def validate!
-	end
-	
-	def valid?
-	end
+  def validate
+  end
+
+  def validate!
+  end
+
+  def valid?
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -59,11 +69,18 @@ if __FILE__ == $PROGRAM_NAME
   t = Test.new
 
   t.test = "123"
-  puts t.test
-
   t.test = "456"
 
-  puts "History"
   puts t.test_history
+
+  Test.strong_attr_acessor(:strong, String)
+
+  t = Test.new
+
+  t.strong = "test"
+  puts t.strong
+
+  t.strong = 123
+  puts t.strong
 
 end
