47fa226383e85bdf695786c2f2282db16a3c5c7f
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-02-06 10:22:55 +0300

    Stash changes

--- Lesson9/validation.rb
+++ Lesson9/validation.rb
@@ -5,29 +5,57 @@ module Validation
   end
 
   module ClassMethods
+    @validators = []
     def validate(name, type, *args)
-      case type
-        when :presence
-        when :format
-
-        when :type
-
-      end
-
+      @validators ||= []
+      @validators << name
     end
 
-    module InstanceMethods
-      def validate!
-        fail 'name' if name.to_s == ''
-        true
-      end
+    def validators
+      @validators ||= []
+    end
+  end
 
-      def valid?
-        validate!
-      rescue
-        false
+
+  module InstanceMethods
+    def validate!
+      # fail 'name' if name.to_s == ''
+      self.class.validators.each do |name, type, *args|
+        v = instance_variable_get("@#{name}")
+        case type
+          when :presence
+            raise "empty" if v.nil?
+          when :format
+            raise "format" if v !~ args.first
+          when :type
+            raise "type" unless v.instance_of?(args.first)
+        end
       end
+      true
+    end
+
+    def valid?
+      validate!
+    rescue
+      false
     end
   end
 end
+
+class A
+  include Validation
+
+  validate :x, :test
+  validate :y, :test
+
+  def test
+    puts self.class.validators
+  end
+
+end
+
+a = A.new
+a.test
+
+a.validate!
 
